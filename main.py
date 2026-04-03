5594fb6ba8de718a27a8155c6bd63f87db3523d5
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2026-04-02 20:24:57 -0600

    Minor updates to main.py's bringup

--- main.py
+++ main.py
@@ -4,25 +4,30 @@ import argparse
 import os
 import traceback
 import platform
+import logging
+logger = logging.getLogger(__name__)
 parser = argparse.ArgumentParser(
     prog="PyWeMoGUI",
-    description="A simple GUI that lets you manage your Belkin WeMo devices."
+    description="A simple GUI that lets you manage your Belkin WeMo devices.",
+    color=True
     )
 parser.add_argument('--loglevel', '-l', help='Optionally define how verbose logs should be (default is INFO)', type=str, choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='INFO')
 args = parser.parse_args()
 
-import logging
-logger = logging.getLogger(__name__)
+# Logger configuration and basic system info retrieval
+#TODO Optional argument for logging to file
 logging.basicConfig(level=f"{args.loglevel}", format='%(asctime)s %(name)s %(levelname)s: %(message)s', datefmt='%I:%M:%S%p')
-logger.info(f"This system is running {platform.system()} {platform.version()} ({platform.machine()}, {os.cpu_count()}x)")
-logger.debug("Logger is up, importing the rest of the required modules...")
+logger.info(f"Logger started, loglevel is set to {args.loglevel}")
+logger.info(f"Running on {platform.system()} {platform.version()} ({platform.machine()}, {os.cpu_count()}x) with Python {platform.python_version()}")
+logger.debug(f"Importing the rest of the required modules...")
 
+# Try to import tkinter (because Python on some platforms lacks it, especially macOS)
 try:
     import tkinter as tk
     import tkinter.ttk as ttk # we don't use this right here but still worth trying to import it for error handling if it's not present
 except ImportError as ie:
     logger.critical(f"Failed to import tkinter. Please check if you need a different Python distribution or maybe just need to install Tkinter. (Full: {repr(ie)})")
-    exit(1)
+    exit(1) # bail lol
 from PyWeMoGUIApp import PyWeMoGUIApp
 
 
@@ -33,7 +38,8 @@ if __name__ == '__main__':
         app = PyWeMoGUIApp(root)
         app.root.mainloop()
     except Exception as e:
-        logger.error(f"Failed to initialize PyWeMoGUIApp: {repr(e)}\n                   {traceback.format_exc()}")
+        logger.critical(f"Failed to initialize PyWeMoGUIApp: {repr(e)}\n                   {traceback.format_exc()}")
+        exit(1)
 else:
     logger.critical("PyWeMoGUI is not designed to be used as a module. Please run it directly instead.")
     exit(1)
