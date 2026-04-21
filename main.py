eb6e97db3b6aed11e8896002497f3dca5dd87929
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2026-04-21 15:31:51 -0600

    Detect and log if running as packaged release

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 print("PyWeMoGUI by ThatStella7922 (https://thatstel.la)")
 
 import argparse
+import sys
 import os
 import traceback
 import platform
@@ -13,11 +14,16 @@ parser = argparse.ArgumentParser(
 parser.add_argument('--loglevel', '-l', help='Optionally define how verbose logs should be (default is INFO)', type=str, choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default='INFO')
 args = parser.parse_args()
 
+is_frozen_binary = getattr(sys, 'frozen', False)
+
 # Logger configuration and basic system info retrieval
 #TODO Optional argument for logging to file
 logging.basicConfig(level=f"{args.loglevel}", format='%(asctime)s %(name)s %(levelname)s: %(message)s', datefmt='%I:%M:%S%p')
 logger.info(f"Logger started, loglevel is set to {args.loglevel}")
-logger.info(f"Running on {platform.system()} {platform.version()} ({platform.machine()}, {os.cpu_count()}x) with Python {platform.python_version()}")
+if is_frozen_binary:
+    logger.info(f"Running on {platform.system()} {platform.release()} ({platform.machine()}) as a packaged release built with Python {platform.python_version()}")
+else:
+    logger.info(f"Running on {platform.system()} {platform.release()} ({platform.machine()}) with Python {platform.python_version()}")
 logger.debug(f"Importing the rest of the required modules...")
 
 # Try to import tkinter (because Python on some platforms lacks it, especially macOS)
@@ -34,16 +40,17 @@ try:
         from hidpi_tk import DPIAwareTk
     from PyWeMoGUIApp import PyWeMoGUIApp
 except ImportError as ie:
-    logger.critical(f"Importing a required library failed! {ie}. If you are running a binary from the Releaes, create a GitHub issue. Otherwise, check your installation!")
+    logger.critical(f"Importing a required library failed! {ie}. If you are running a binary from the Releases, create a GitHub issue to report this bug. Otherwise, check your installation!")
     exit(1)
 
 if __name__ == '__main__':
     logger.debug("Setting up Tk and initializing PyWeMoGUIApp...")
     try:
         if platform.system() == "Windows":
-            root = DPIAwareTk()
+            root = DPIAwareTk() ### supposedly, DPIAwareTk is supposed to fallback to regular Tk if not running on Windows but it crashed on macOS so I am checking explicitly for Windows before using
         else:
-            root = tk.Tk()
+            root = tk.Tk() ### not on Windows, use regular Tk (*thanks* for nothing hidpit_tk)
+
         app = PyWeMoGUIApp(root)
         app.root.mainloop()
     except Exception as e:
