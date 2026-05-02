85672db99c9384cddbb658b03e89e516bca5afa8
ThatStella7922 <10524728+ThatStella7922@users.noreply.github.com>
2026-05-01 18:07:38 -0600

    Work on dialogs for future use
    Add about dialog (should be fine for now) and a help browser dialog that needs to be finished (or rather started at all)

--- PyWeMoGUIDialogs.py
+++ PyWeMoGUIDialogs.py
@@ -6,8 +6,13 @@ import qrcode
 from PIL import Image, ImageTk
 
 class PyWeMoGUIHomeKitInfoDialog(tk.Toplevel):
+    '''This dialog provides nice-looking HomeKit information to the user about their WeMo.
+    
+    Provide the device name, setup URI and setup code and this dialog will automatically format the info to look nice.
+    
+    Setup URI will be turned into a QR code for scanning and the setup code will be displayed with a "copy to clipboard" button.'''
     def __init__(self, parent, deviceName:str, setupURI: str, setupCode: str):
-        self.logger = logging.getLogger(__name__)
+        self.logger = logging.getLogger(f"{__name__} HomeKitInfoDialog")
 
         # window setup
         self.logger.debug("Initializing homekit info dialog now")
@@ -41,3 +46,30 @@ class PyWeMoGUIHomeKitInfoDialog(tk.Toplevel):
             self.logger.info("HomeKit setup code copied to clipboard")
         except Exception as e:
             messagebox.showerror("Error", f"Could not copy IP address because of the following error:\n{repr(e)}")
+
+class PyWeMoGUIAboutDialog(tk.Toplevel):
+    '''probably in the future going to be a nice about dialog that's fancy with an image for a potential pywemogui logo'''
+    def __init__(self, parent):
+        self.logger = logging.getLogger(f"{__name__} AboutDialog")
+
+        # window setup
+        super().__init__(parent)
+        self.title("About PyWeMoGUI")
+        self.geometry("400x200")
+        self.resizable(False, False)
+
+        about_text = "PyWeMoGUI\nA simple GUI that lets you manage your Belkin WeMo devices.\n\nPyWeMoGUI is built on the pyWeMo library and is not supported or endorsed by pyWeMo contributors.\n\nhttps://github.com/ThatStella7922/PyWeMoGUI\nhttps://thatstel.la\nThatStella7922 2026"
+        about_label = ttk.Label(self, wraplength=380 ,text=about_text)
+        about_label.pack(padx=10, pady=10)
+
+class PyWeMoGUIHelpDialog(tk.TopLevel):
+    '''Full help dialog with tabs covering different help topics'''
+    def __init__(self, parent):
+        self.logger = logging.getLogger(f"{__name__} HelpDialog")
+
+        #set the window up
+        super().__init__(parent)
+        self.title("PyWeMoGUI Help")
+
+        pass
+        #TODO: Finish this help dialog, figure out where it's going to pull help data from, figure out how to display it
